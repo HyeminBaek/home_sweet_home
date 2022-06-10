27a277b79b9134d08bb174e7df88104ddf73c714
bacccine <55080554+HyeminBaek@users.noreply.github.com>
2022-06-10 13:15:06 +0900

    Update justmouse.java

--- moving mouse/src/com/hammm/justmouse.java
+++ moving mouse/src/com/hammm/justmouse.java
@@ -1,8 +1,9 @@
-package com.hammm;
+package justMouse;
 
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.InputEvent;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class justMouse {
@@ -11,15 +12,37 @@ public class justMouse {
 		Robot robot = new Robot();
 		Random random = new Random();
 		
+		System.out.println("start");
 		while(true) {
+			//마우스 랜덤 이동
 			int x = random.nextInt()%1280;
 			int y = random.nextInt()%960;
 			robot.mouseMove(x, y);
+			
+			//마우스 좌 클릭
+			robot.mousePress(InputEvent.BUTTON1_MASK);
+			Thread.sleep(1000);
+			robot.mouseRelease(InputEvent.BUTTON1_MASK);
+			
+			//키보드 입력
+			ArrayList<Character> keyInput = new ArrayList<Character>();
+			keyInput.add('h'); keyInput.add('i');
+			
+			for(int i=0;i<keyInput.size();i++)
+				robot.keyPress(keyInput.get(i));
+			
+			//마우스 우 클릭
 			robot.mousePress(InputEvent.BUTTON3_MASK);
+			Thread.sleep(1000);
 			robot.mouseRelease(InputEvent.BUTTON3_MASK);
 			
+			//마우스 랜덤 이동
+			x = random.nextInt()%1280;
+			y = random.nextInt()%960;
+			robot.mouseMove(x, y);
+			
 			Thread.sleep(1000*60*10);
-			System.out.println("start");
+			System.out.println("move");
 		}
 	}
 
